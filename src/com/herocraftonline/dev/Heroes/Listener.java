57ebee6f6afdfd26c44ad5ca8e7eefbc76faedce
RightLegRed <nath-daws@hotmail.co.uk>
2011-03-09 21:57:10 +0000

    Started project, folder system set up (changes may come soon), main class and listener has been made.

---
+++ src/com/herocraftonline/dev/Heroes/Listener.java
@@ -0,0 +1,13 @@
+package com.herocraftonline.dev.Heroes;
+
+import org.bukkit.event.player.PlayerListener;
+
+public class Listener extends PlayerListener {
+	private final Heroes plugin;
+	
+	public Listener(Heroes instance) {
+		 plugin = instance;
+	}
+	
+
+}
