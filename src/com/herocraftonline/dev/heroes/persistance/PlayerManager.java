391cd57e25ebf6cf42f9e3365ea5822890c38003
Rigby <rigby@onarandombox.com>
2011-03-21 10:37:28 +0000

    Tidy Up + Commenting

--- src/com/herocraftonline/dev/heroes/persistance/PlayerManager.java
+++ src/com/herocraftonline/dev/heroes/persistance/PlayerManager.java
@@ -17,32 +17,50 @@ import com.herocraftonline.dev.heroes.util.Properties;
  */
 public class PlayerManager {
 
+    /**
+     * Grab the given Players current Experience.
+     * @param p
+     * @return
+     */
     public static int getExp(Player p) {
-        ResultSet pRS = Heroes.sql.trySelect("SELECT * FROM players WHERE name='" + p.getName() + "'");
+        String name = p.getName(); // Grab the players name.
+        int value = -1;
         try {
-            if (pRS.getFetchSize() > 0) {
-                return pRS.getInt("exp");
-            } else {
-                return -1;
-            }
+            Connection conn = Heroes.sql.getConnection();
+            Statement s = conn.createStatement();
+            ResultSet r = s.executeQuery("SELECT * FROM players WHERE name='" + name + "'");
+            r.next();
+            value = r.getInt("exp");
+            r.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return -1;
+        return value;
     }
-
+    
+    /**
+     * Set the given Players Exp to the value given.
+     * @param p
+     * @param e
+     * @throws Exception
+     */
     public static void setExp(Player p, Integer e) throws Exception {
         String n = p.getName();
         Heroes.sql.tryUpdate("UPDATE players SET `exp`=" + e + " WHERE `name`=" + n);
     }
 
+    /**
+     * Grab the given Players Class.
+     * @param p
+     * @return
+     */
     public static String getClass(Player p) {
         String pClass = null;
-
+        String name = p.getName();
         try {
             Connection conn = Heroes.sql.getConnection();
             Statement s = conn.createStatement();
-            ResultSet r = s.executeQuery("SELECT * FROM players");
+            ResultSet r = s.executeQuery("SELECT * FROM players WHERE name='" + name + "'");
             r.next();
             pClass = r.getString("class");
             r.close();
@@ -53,37 +71,44 @@ public class PlayerManager {
         return pClass;
     }
 
+    /**
+     * Change the Players Class to the specified Class.
+     * @param p
+     * @param c
+     */
     public static void setClass(Player p, String c) {
         String n = p.getName();
         Heroes.sql.tryUpdate("UPDATE players SET class='" + c + "' WHERE name='" + n + "'");
     }
 
+    /**
+     * Insert the Player into the Database with the default class.
+     * @param p
+     */
     public static void newPlayer(Player p) {
         String n = p.getName();
-        Heroes.sql.tryUpdate("INSERT INTO 'players' (name, class, exp, mana) VALUES ('" + n + "','Vagrant', '0', '0') ");
+        Heroes.sql.tryUpdate("INSERT INTO 'players' (name, class, exp, mana) VALUES ('" + n + "','Vagrant', '0', '0') "); // Vargrant needs to change, *customizeable*
     }
 
+    /**
+     * Check if the Player already exists within the Database.
+     * @param n
+     * @return
+     */
     public static boolean checkPlayer(String n) {
-        int count = 0;
-
-        try {
-            Connection conn = Heroes.sql.getConnection();
-            Statement s = conn.createStatement();
-            ResultSet r = s.executeQuery("SELECT COUNT(*) AS rowcount FROM players WHERE name='" + n + "'");
-            r.next();
-            count = r.getInt("rowcount");
-            r.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        if (count > 0) {
+        String query = "SELECT COUNT(*) AS rowcount FROM players WHERE name='" + n + "'";
+        if (Heroes.sql.rowCount(query) > 0) {
             return true;
         } else {
             return false;
         }
     }
 
+    /**
+     * Convert the given Exp into the correct Level.
+     * @param exp
+     * @return
+     */
     public static int getLevel(Integer exp) {
         int n = 0;
         for (Integer i : Properties.level) {
