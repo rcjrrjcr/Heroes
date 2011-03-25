fe1dc2b88ad82dd5b50a29f93cf5c0cd492e36f7
RightLegRed <nath-daws@hotmail.co.uk>
2011-03-25 01:19:49 +0000

    Removed static from variables in Properties. New API class

--- src/com/herocraftonline/dev/heroes/util/Properties.java
+++ src/com/herocraftonline/dev/heroes/util/Properties.java
@@ -8,33 +8,37 @@ import org.bukkit.Material;
 import org.bukkit.entity.CreatureType;
 
 public class Properties {
-    
-    // Leveling//
-    public static double power;
-    public static int baseExp;
-    public static int maxExp;
-    public static int maxLevel;
-    public static ArrayList<Integer> level = new ArrayList<Integer>();
-    // Experience//
-    public static int playerKillingExp = 0;
-    public static HashMap<CreatureType, Integer> creatureKillingExp = new HashMap<CreatureType, Integer>();
-    public static HashMap<Material, Integer> miningExp = new HashMap<Material, Integer>();
-    public static int loggingExp = 0;
-    // Default//
-    public static String defClass;
-    public static int defLevel;
-    // Properties//
-    public static boolean iConomy;
-    public static ChatColor cColor;
-    public static String prefix;
-    public static int swapcost;
 
-    public static void calcExp() {
-        level.clear();
-        double A = 2 * (maxExp - baseExp) * Math.pow(maxLevel, (power * -1));
-        for (int n = 1; n <= maxLevel; n++) {
-            level.add((int) (A / 2 * Math.pow(n - 1, power) + baseExp));
-        }
-    }
-    
+	// Leveling//
+	public double power;
+	public int baseExp;
+	public int maxExp;
+	public int maxLevel;
+	public ArrayList<Integer> level = new ArrayList<Integer>();
+	// Experience//
+	public int playerKillingExp = 0;
+	public HashMap<CreatureType, Integer> creatureKillingExp = new HashMap<CreatureType, Integer>();
+	public HashMap<Material, Integer> miningExp = new HashMap<Material, Integer>();
+	public int loggingExp = 0;
+	// Default//
+	public String defClass;
+	public int defLevel;
+	// Properties//
+	public boolean iConomy;
+	public ChatColor cColor;
+	public String prefix;
+	public int swapcost;
+
+	
+	/**
+	 * Generate experience for the level ArrayList<Integer>
+	 */
+	public void calcExp() {
+		level.clear();
+		double A = 2 * (maxExp - baseExp) * Math.pow(maxLevel, (power * -1));
+		for (int n = 1; n <= maxLevel; n++) {
+			level.add((int) (A / 2 * Math.pow(n - 1, power) + baseExp));
+		}
+	}
+
 }
