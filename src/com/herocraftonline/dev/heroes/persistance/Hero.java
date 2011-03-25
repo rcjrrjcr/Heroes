89d92ef52b9fbb79bbef900b596c83dff43be6cc
Rigby <rigby@onarandombox.com>
2011-03-25 21:08:25 +0000

    Basic 'Hero' Class

---
+++ src/com/herocraftonline/dev/heroes/persistance/Hero.java
@@ -0,0 +1,55 @@
+package com.herocraftonline.dev.heroes.persistance;
+
+import org.bukkit.entity.Player;
+
+import com.herocraftonline.dev.heroes.classes.HeroClass;
+
+public class Hero {
+
+    public Player player;
+    private HeroClass playerClass;
+    private HeroClass playerSubClass;
+    private int experience;
+    private int mana;
+
+    // Constructor
+    public Hero(Player p, HeroClass pClass, HeroClass pSubClass, int exp, int mana){
+        this.player = p;
+        this.playerClass = pClass;
+        this.playerSubClass = pSubClass;
+        this.experience = exp;
+        this.mana = mana;
+    }
+
+    public HeroClass getPlayerClass() {
+        return playerClass;
+    }
+
+    public HeroClass getPlayerSubClass() {
+        return playerSubClass;
+    }
+
+    public int getExp() {
+        return experience;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+    
+    public void setPlayerClass(HeroClass heroclass) {
+        playerClass = heroclass;
+    }
+
+    public void setPlayerSubClass(HeroClass heroclass) {
+        playerSubClass = heroclass;
+    }
+
+    public void setExperience(int value) {
+        experience = value;
+    }
+
+    public void setMana(int value) {
+        mana = value;
+    }
+}
