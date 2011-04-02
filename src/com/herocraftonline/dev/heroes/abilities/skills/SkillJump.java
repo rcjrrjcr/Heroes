9cfa4c20c77bef842c908002709677bebc76baee
RightLegRed <nath-daws@hotmail.co.uk>
2011-04-03 00:00:05 +0100

    Added SkillJump

---
+++ src/com/herocraftonline/dev/heroes/abilities/skills/SkillJump.java
@@ -0,0 +1,38 @@
+package com.herocraftonline.dev.heroes.abilities.skills;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.herocraftonline.dev.heroes.Heroes;
+import com.herocraftonline.dev.heroes.classes.HeroClass;
+import com.herocraftonline.dev.heroes.command.BaseCommand;
+import com.herocraftonline.dev.heroes.persistence.Hero;
+
+public class SkillJump extends BaseCommand {
+
+    // TODO: Register this command in Heroes
+    public SkillJump(Heroes plugin) {
+        super(plugin);
+        name = "Jump";
+        description = "Skill - Jump";
+        usage = "/jump";
+        minArgs = 0;
+        maxArgs = 0;
+        identifiers.add("jump");
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        if (sender instanceof Player) {
+            Hero hero = plugin.getHeroManager().getHero((Player) sender);
+            HeroClass heroClass = plugin.getClassManager().getClass(hero.toString());
+            // TODO: Check for CD time left, if 0 execute.
+            if (!(plugin.getClassManager().getClass(plugin.getHeroManager().getHero((Player) sender).getClass().toString()).getSpells().contains("Jump"))) {
+                plugin.getMessaging().send(sender, "Sorry, $1, that ability isn't for your class!", ((Player) sender).getName());
+                return;
+            }
+            
+            hero.getPlayer().setVelocity(hero.getPlayer().getVelocity().setY((hero.getPlayer().getVelocity().getY()*2)));
+        }
+    }
+}
