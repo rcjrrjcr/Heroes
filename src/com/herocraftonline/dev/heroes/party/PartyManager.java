aa1f1c7840d3f910713d305802075e31e73dcf79
RightLegRed <nath-daws@hotmail.co.uk>
2011-04-06 02:51:40 +0100

    Finished HeroParty and PartyManager

--- src/com/herocraftonline/dev/heroes/party/PartyManager.java
+++ src/com/herocraftonline/dev/heroes/party/PartyManager.java
@@ -3,33 +3,47 @@ package com.herocraftonline.dev.heroes.party;
 import java.util.HashSet;
 import java.util.Set;
 
+
+
 import org.bukkit.entity.Player;
 
 import com.herocraftonline.dev.heroes.Heroes;
-import com.herocraftonline.dev.heroes.persistence.Hero;
 
 
 
 public class PartyManager {
-    
+
     private Heroes plugin;
     private Set<HeroParty> parties;
-    
+
     public PartyManager(Heroes plugin){
         this.plugin = plugin;
         this.parties = new HashSet<HeroParty>();
     }
-    
+
     public HeroParty createHeroParty(Player leader){
         return new HeroParty(leader);
     }
-    
+
     public void addHeroParty(HeroParty party){
         parties.add(party);
     }
-    
+
     public void removeHeroParty(HeroParty party){
         parties.remove(party);
     }
+    
+    public HeroParty getHeroParty(Player owner){
+        for(HeroParty party : parties){
+            if(party.getLeader().equals(owner)) continue;
+            return party;
+        }
+        return null;
+    }
+    
+    public Set<HeroParty> getHeroParties(){
+        return parties;
+    }
+    
     
 }
