f8018be9bdf0b8515e5d95c1adf931fb1f4e7e69
RightLegRed <nath-daws@hotmail.co.uk>
2011-04-17 23:07:13 +0100

    Added experience share

--- src/com/herocraftonline/dev/heroes/HCustomEventListener.java
+++ src/com/herocraftonline/dev/heroes/HCustomEventListener.java
@@ -1,6 +1,7 @@
 package com.herocraftonline.dev.heroes;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.CustomEventListener;
 import org.bukkit.event.Event;
 
@@ -21,6 +22,13 @@ public class HCustomEventListener extends CustomEventListener {
             BlockBreakExperienceEvent e = (BlockBreakExperienceEvent) event;
             Properties prop = plugin.getConfigManager().getProperties();
             Hero hero = plugin.getHeroManager().getHero(e.getPlayer());
+            if(hero.getParty().getExp() == true){
+                for(Player p : hero.getParty().getMembers()){
+                    Hero nHero = plugin.getHeroManager().getHero(p);
+                    nHero.setExperience(nHero.getExperience() + (e.getExp() / hero.getParty().getMembers().size()));
+                }
+                e.setExp(e.getExp() / hero.getParty().getMembers().size());
+            }
             if (prop.getLevel(hero.getExperience()) != prop.getLevel(hero.getExperience() + e.getExp())) {
                 e.getPlayer().sendMessage(ChatColor.RED + "You just reached level" + ChatColor.BLUE + prop.getLevel(hero.getExperience() + e.getExp()));
             }
@@ -29,6 +37,13 @@ public class HCustomEventListener extends CustomEventListener {
             KillExperienceEvent e = (KillExperienceEvent) event;
             Properties prop = plugin.getConfigManager().getProperties();
             Hero hero = plugin.getHeroManager().getHero(e.getPlayer());
+            if(hero.getParty().getExp() == true){
+                for(Player p : hero.getParty().getMembers()){
+                    Hero nHero = plugin.getHeroManager().getHero(p);
+                    nHero.setExperience(nHero.getExperience() + (e.getExp() / hero.getParty().getMembers().size()));
+                }
+                e.setExp(e.getExp() / hero.getParty().getMembers().size());
+            }
             if (prop.getLevel(hero.getExperience()) != prop.getLevel(hero.getExperience() + e.getExp())) {
                 e.getPlayer().sendMessage(ChatColor.RED + "You just reached level" + ChatColor.BLUE + prop.getLevel(hero.getExperience() + e.getExp()));
             }
