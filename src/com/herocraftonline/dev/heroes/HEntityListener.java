def01b5d2a443206f82a8a2ee8c7e5901a546a96
DThielke <dave.thielke@gmail.com>
2011-04-24 04:02:16 -0400

    Moved Bladegasp's event handling into its class.

--- src/com/herocraftonline/dev/heroes/HEntityListener.java
+++ src/com/herocraftonline/dev/heroes/HEntityListener.java
@@ -10,7 +10,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageByProjectileEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityListener;
 
@@ -122,13 +121,5 @@ public class HEntityListener extends EntityListener {
                 }
             }
         }
-        if (defender instanceof Player) {
-            if (plugin.getHeroManager().getHero((Player) defender).getEffects().get("Bladegrasp") != null) {
-                if (event.getCause() == DamageCause.ENTITY_ATTACK || event.getCause() == DamageCause.ENTITY_EXPLOSION) {
-                    event.setCancelled(true);
-                }
-            }
-        }
-
     }
 }
--- src/com/herocraftonline/dev/heroes/command/skill/skills/SkillBladegrasp.java
+++ src/com/herocraftonline/dev/heroes/command/skill/skills/SkillBladegrasp.java
@@ -1,5 +1,13 @@
 package com.herocraftonline.dev.heroes.command.skill.skills;
 
+import java.util.Map;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.EntityListener;
+
 import com.herocraftonline.dev.heroes.Heroes;
 import com.herocraftonline.dev.heroes.command.skill.ActiveSkill;
 import com.herocraftonline.dev.heroes.persistence.Hero;
@@ -20,5 +28,24 @@ public class SkillBladegrasp extends ActiveSkill {
     public boolean use(Hero hero, String[] args) {
         hero.getEffects().put(name, System.currentTimeMillis() + 60000.0);
         return true;
+    }
+    
+    public class SkillEntityListener extends EntityListener {
+        
+        @Override
+        public void onEntityDamage(EntityDamageEvent event) {
+            Entity defender = event.getEntity();
+            if (defender instanceof Player) {
+                Player player = (Player) defender;
+                Map<String, Double> effects = plugin.getHeroManager().getHero(player).getEffects();
+                if (effects.containsKey(name)) {
+                    if (event.getCause() == DamageCause.ENTITY_ATTACK || event.getCause() == DamageCause.ENTITY_EXPLOSION) {
+                        event.setCancelled(true);
+                        effects.remove(name);
+                    }
+                }
+            }
+        }
+        
     }
 }
