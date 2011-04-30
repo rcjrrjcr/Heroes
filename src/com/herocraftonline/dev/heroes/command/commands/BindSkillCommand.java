f01ca6b5d9210dd69b6449ebaf682c6779178e2d
DThielke <dave.thielke@gmail.com>
2011-04-29 23:13:56 -0400

    Made the responses from /bind more descriptive.

---
+++ src/com/herocraftonline/dev/heroes/command/commands/BindSkillCommand.java
@@ -0,0 +1,46 @@
+package com.herocraftonline.dev.heroes.command.commands;
+
+import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.herocraftonline.dev.heroes.Heroes;
+import com.herocraftonline.dev.heroes.classes.HeroClass;
+import com.herocraftonline.dev.heroes.command.BaseCommand;
+import com.herocraftonline.dev.heroes.persistence.Hero;
+import com.herocraftonline.dev.heroes.util.MaterialUtil;
+import com.herocraftonline.dev.heroes.util.Messaging;
+
+public class BindSkillCommand extends BaseCommand {
+
+    public BindSkillCommand(Heroes plugin) {
+        super(plugin);
+        name = "BindSkill";
+        description = "Binds a skill with an item";
+        usage = "/bind <skill>";
+        minArgs = 0;
+        maxArgs = 1000;
+        identifiers.add("bind");
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            Hero hero = plugin.getHeroManager().getHero(player);
+            HeroClass heroClass = hero.getPlayerClass();
+            Material material = player.getItemInHand().getType();
+            if (args.length > 0) {
+                if (heroClass.hasSkill(args[0])) {
+                    hero.bind(material, args);
+                    Messaging.send(sender, "$1 has been bound to $2.", MaterialUtil.getFriendlyName(material), args[0]);
+                } else {
+                    Messaging.send(sender, "That skill does not exist for your class.");
+                }
+            } else {
+                hero.unbind(material);
+                Messaging.send(sender, "$1 is no longer bound to a skill.", MaterialUtil.getFriendlyName(material));
+            }
+        }
+    }
+}
