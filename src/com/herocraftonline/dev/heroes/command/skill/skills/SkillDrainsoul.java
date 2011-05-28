66eb1a28113b00718701ee925fa4df3b46af7756
RightLegRed <nath-daws@hotmail.co.uk>
2011-05-28 10:27:33 +0100

    Skill - Drainsoul

---
+++ src/com/herocraftonline/dev/heroes/command/skill/skills/SkillDrainsoul.java
@@ -0,0 +1,43 @@
+package com.herocraftonline.dev.heroes.command.skill.skills;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.util.config.ConfigurationNode;
+
+import com.herocraftonline.dev.heroes.Heroes;
+import com.herocraftonline.dev.heroes.command.skill.TargettedSkill;
+import com.herocraftonline.dev.heroes.persistence.Hero;
+
+public class SkillDrainsoul extends TargettedSkill{
+
+    public SkillDrainsoul(Heroes plugin) {
+        super(plugin);
+        name = "Drainsoul";
+        description = "Absorb health from target";
+        minArgs = 0;
+        maxArgs = 0;
+        identifiers.add("skill stoneskin");
+    }
+    
+    @Override
+    public ConfigurationNode getDefaultConfig() {
+        ConfigurationNode node = super.getDefaultConfig();
+        node.setProperty("absorb-amount", 4);
+        return node;
+    }
+
+    @Override
+    public boolean use(Hero hero, LivingEntity target, String[] args) {
+        Player player = hero.getPlayer();
+        int absorbamount = getSetting(hero.getHeroClass(), "absorb-amount", 4);
+        if(target.getHealth() < absorbamount){
+            player.setHealth(player.getHealth() + target.getHealth());
+            target.damage(target.getHealth());
+        }else{
+            player.setHealth(player.getHealth() + absorbamount);
+            target.damage(absorbamount);  
+        }
+        return true;
+    }
+
+}
