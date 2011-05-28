570d6ad4ec4f96f19be1f41801b9fdcd0607991f
RightLegRed <nath-daws@hotmail.co.uk>
2011-05-28 11:13:46 +0100

    Skill - Charge

---
+++ src/com/herocraftonline/dev/heroes/command/skill/skills/SkillCharge.java
@@ -0,0 +1,27 @@
+package com.herocraftonline.dev.heroes.command.skill.skills;
+
+import org.bukkit.entity.LivingEntity;
+
+import com.herocraftonline.dev.heroes.Heroes;
+import com.herocraftonline.dev.heroes.command.skill.TargettedSkill;
+import com.herocraftonline.dev.heroes.persistence.Hero;
+
+public class SkillCharge extends TargettedSkill{
+
+    public SkillCharge(Heroes plugin) {
+        super(plugin);
+        name = "Charge";
+        description = "Charges towards your target";
+        usage = "/skill charge";
+        minArgs = 0;
+        maxArgs = 0;
+        identifiers.add("skill charge");
+    }
+
+    @Override
+    public boolean use(Hero hero, LivingEntity target, String[] args) {
+        hero.getPlayer().teleport(target);
+        return true;
+    }
+
+}
