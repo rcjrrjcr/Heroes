596bd6a9bba88f5e134429e60cc3bd636831e07d
Rigby <rigby@onarandombox.com>
2011-06-05 22:38:11 +0100

    Cleaned up formatting You need to return true so mana is consumed and the skill is put on cooldown, if the spell is unsuccessful you return false.

--- src/com/herocraftonline/dev/heroes/command/skill/skills/SkillBarrage.java
+++ src/com/herocraftonline/dev/heroes/command/skill/skills/SkillBarrage.java
@@ -1,4 +1,4 @@
-package com.scswc.shadrxninga.skillbarrage;
+package com.herocraftonline.dev.heroes.command.skill.skills;
 
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
@@ -7,27 +7,26 @@ import com.herocraftonline.dev.heroes.Heroes;
 import com.herocraftonline.dev.heroes.command.skill.ActiveSkill;
 import com.herocraftonline.dev.heroes.persistence.Hero;
 
+public class SkillBarrage extends ActiveSkill {
 
-public class SkillBarrage extends ActiveSkill{
+    public SkillBarrage(Heroes plugin) {
+        super(plugin);
+        name = "Barrage";
+        description = "Fire a Barrage of Arrows around you.";
+        usage = "/skill barrage";
+        minArgs = 0;
+        maxArgs = 0;
+        identifiers.add("skill barrage");
+    }
 
-	public SkillBarrage(Heroes plugin) {
-		super(plugin);
-		name = "Barrage";
-		description = "Fire a Barrage of Arrows from you";
-		usage = "/skill barrgae";
-		minArgs = 0;
-		maxArgs = 0;
-		identifiers.add("skill barrage");
-	}
-	
-	public boolean use(Hero hero, String[] args){
-		Player player = hero.getPlayer();
-				double diff = (2 * Math.PI) / 24.0;
-	            for (double a = 0; a < 2 * Math.PI; a += diff) {
-	                Vector vel = new Vector(Math.cos(a), 0, Math.sin(a));
-	                player.shootArrow().setVelocity(vel);
-	            }
-				return false;
-
-}
+    @Override
+    public boolean use(Hero hero, String[] args) {
+        Player player = hero.getPlayer();
+        double diff = (2 * Math.PI) / 24.0;
+        for (double a = 0; a < 2 * Math.PI; a += diff) {
+            Vector vel = new Vector(Math.cos(a), 0, Math.sin(a));
+            player.shootArrow().setVelocity(vel);
+        }
+        return true;
+    }
 }
